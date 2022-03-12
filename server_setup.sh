fb46965888912ed1aab418f3bdf8cda1f72b8eda
Schen <schen.git@gmail.com>
2022-03-12 23:30:43 +0900

    update setup script

--- server_setup.sh
+++ server_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 sleep 5s
 
@@ -29,21 +29,36 @@ EOF
 
 chmod u+x $SWAP_ON_SCRIPT && bash $SWAP_ON_SCRIPT
 
+# VOLATILE
 cat << 'EOF' >> ~/.bashrc
 export HOST_NET_INTERFACE=enp1s0
 
 HOST_IP=$(ip addr | grep $HOST_NET_INTERFACE | grep inet | awk -F " brd" '{print $1}' | awk -F "inet " '{print $2}' | cut -d '/' -f 1)
 export HOST_IP=$HOST_IP
 
+cd /srv/docker-portable-OVPN
 EOF
 
 # Allowing Incoming HTTP && HTTPS
-iptables -C INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT {
+iptables -C INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT 2>/dev/null || {
     iptables -A INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 }
-iptables -C OUTPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT{
+iptables -C OUTPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT 2>/dev/null || {
     iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 }
 
 # OPENVPN SETUP
 mkdir -p /out
+
+# if dir not exist, then init
+D0=/srv/docker-portable-OVPN
+[[ -d D0 ]] || git clone https://github.com/schen0x/docker-portable-OVPN /srv/docker-portable-OVPN
+
+# VOLATILE
+HOST_NET_INTERFACE=enp1s0
+export HOST_IP=$(ip addr | grep $HOST_NET_INTERFACE | grep inet | awk -F " brd" '{print $1}' | awk -F "inet " '{print $2}' | cut -d '/' -f 1)
+
+# refresh the ENVs
+bash && cd /srv/docker-portable-OVPN
+
+docker-compose build && docker-compose up -d
