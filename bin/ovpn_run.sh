0c75a31b4d3a84650c147ddd9680790d82cb537f
Schen <schen.git@gmail.com>
2022-03-12 19:31:26 +0900

    buildtime runtime

--- bin/ovpn_run.sh
+++ bin/ovpn_run.sh
@@ -2,6 +2,7 @@ CADIR=/etc/openvpn-ca
 OHOME=/etc/openvpn
 MOUNTED_HOST_DIR=/out
 HOST_IP=$HOST_IP
+CONTAINER_NET_INTERFACE=eth0
 
 cp $(find $CADIR -type f -name "client.key") $MOUNTED_HOST_DIR
 cp $(find $CADIR -type f -name "client.crt") $MOUNTED_HOST_DIR
@@ -11,5 +12,5 @@ cp $(find $CADIR -type f -name "ta.key") $MOUNTED_HOST_DIR
 sed -i -e "s/<0w0_SERVER_HOST>/$HOST_IP/g" $OHOME/client.example
 cp $OHOME/client.example $MOUNTED_HOST_DIR/client.ovpn
 
-iptables -t nat -C POSTROUTING -s 10.8.0.0 -o $HOST_NET_INTERFACE -j MASQUERADE
-openvpn --config /server.conf
+iptables -t nat -C POSTROUTING -s 10.8.0.0/24 -o $CONTAINER_NET_INTERFACE -j MASQUERADE
+openvpn --config $OHOME/server.conf
