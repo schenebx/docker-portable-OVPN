67b941bd0dcbb8614992f38a137ab75536a0c57f
schenebx <schen.ebx@gmail.com>
2022-04-18 11:42:33 +0900

    update multi-client script

--- bin/ovpn_run.sh
+++ bin/ovpn_run.sh
@@ -3,6 +3,11 @@
 # server global variable
 HOST_IP=$HOST_IP
 
+[[ -z ${HOST_IP} ]] && read -p "Enter the public ip of this instance: " HOST_IP
+
+mkdir -p $MOUNTED_HOST_DIR
+rm -f $MOUNTED_HOST_DIR/*.gz
+
 # defined in `server.conf`
 OVPN_SUBNET=10.8.0.0/24
 
@@ -11,18 +16,23 @@ OHOME=/etc/openvpn
 MOUNTED_HOST_DIR=/out
 CONTAINER_NET_INTERFACE=eth0
 
-TMPFS=$(mktemp -d)
-mkdir -p $MOUNTED_HOST_DIR
+CA_FILE=$(find $CADIR -type f -name "ca.crt")
+TA_FILE=$(find $CADIR -type f -name "ta.key")
 
-cp $(find $CADIR -type f -name "client.key") $TMPFS
-cp $(find $CADIR -type f -name "client.crt") $TMPFS
-cp $(find $CADIR -type f -name "ca.crt") $TMPFS
-cp $(find $CADIR -type f -name "ta.key") $TMPFS
-cp $OHOME/conf/client.example $TMPFS/client.ovpn
-sed -i -e "s/<0w0_SERVER_HOST>/$HOST_IP/g" $TMPFS/client.ovpn
+for clientKeyABS in $(find $CADIR -type f -name "client*.key"); do
+    clientName=$(basename $clientKeyABS | rev | cut -d "." -f 2- | rev)
+    TMPFS=$(mktemp -d)
+    cp $CA_FILE $TMPFS                          # ca.crt
+    cp $TA_FILE $TMPFS                          # ta.key
+    cp $(find $CADIR -type f -name "$clientName.key") $TMPFS # $clientName.key
+    cp $(find $CADIR -type f -name "$clientName.crt") $TMPFS # $clientName.crt
+    cp $OHOME/conf/client.example $TMPFS/$clientName.ovpn
+    sed -i -e "s/<0w0_SERVER_HOST>/$HOST_IP/g" $TMPFS/$clientName.ovpn
+    sed -i -e "s/<0w0_CLIENT_NAME>/$clientName/g" $TMPFS/$clientName.ovpn # $clientName.ovpn
+    tar cvfz $MOUNTED_HOST_DIR/$clientName.gz -C $TMPFS .
+done
 
-rm -f $MOUNTED_HOST_DIR/conn.gz
-tar cvfz $MOUNTED_HOST_DIR/conn.gz -C $TMPFS .
+tar cvfz $MOUNTED_HOST_DIR/all.gz -C $MOUNTED_HOST_DIR .
 
 # Check if rule exist. Error if not exist. Add rule on error
 iptables -t nat -C POSTROUTING -s $OVPN_SUBNET -o $CONTAINER_NET_INTERFACE -j MASQUERADE 2>/dev/null || {
